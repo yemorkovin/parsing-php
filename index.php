f6cd6fb5b7250782ecd520b99f7c073fc5d41e70
Yevhen Morkovin <admin@yemorkovin.ru>
2023-04-25 19:32:07 +0300

    25.04.23

---
+++ index.php
@@ -0,0 +1,8 @@
+﻿<?
+$str = file_get_contents('Лаб_Парсер1.htm');
+preg_match_all('#<p class=\"mtx3\">(.+?)</p>#su', $str, $res);
+foreach($res[1] as $item){
+	file_put_contents('1.txt', preg_replace('/\s+/ui', ' ', $item)."\n", FILE_APPEND);
+}
+file_put_contents('1.txt', count($res[1]), FILE_APPEND);
+?>
